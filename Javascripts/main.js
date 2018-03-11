1a0bca5998c5954d845741b0f949db12f18d813a
Adam Wieckert <wiec1369@gmail.com>
2018-03-11 12:33:49 -0500

    Added Text Area and buttons to index.html. Added the boilerplate for the translator objects array. Added the initial function.

--- Javascripts/main.js
+++ Javascripts/main.js
@@ -1,1 +1,22 @@
 console.log("YAY!");
+
+var testOfText = document.getElementById('wordsNstuff').innerHTML;
+
+console.log("testOfText: ", testOfText);
+
+var translator = [
+    {
+        id: "German",
+    },
+    {
+        id: "Spanish",
+    },
+    {
+        id: "Italian",
+    },
+];
+
+function awesomeTranslator (language) {
+    
+
+}
