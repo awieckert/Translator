d9d42c9b89733c66c7c8ae5ab5e528772e7ccfdc
Adam Wieckert <wiec1369@gmail.com>
2018-03-26 20:00:05 -0500

    Wrote the translator without an If statement

--- Javascripts/main.js
+++ Javascripts/main.js
@@ -1,69 +1,5 @@
-// console.log("YAY!");
 
-// var testOfText = document.getElementById('wordsNstuff').innerHTML;
-
-// console.log("testOfText: ", testOfText);
-
-// Merry, Christmas, Happy, New, Year, Have, A, And, Xmas, Holiday, We, Wish, You
-
-// var translator = [
-//     {
-//         id: "German",
-//         we: "wir",
-//         wish: "wunsch",
-//         you: "sie",
-//         a: "ein",
-//         merry: "frolich",
-//         christmas: "weihnachten",
-//         happy: "gulcklich",
-//         new: "neu",
-//         year: "jahr",
-//     },
-//     {
-//         id: "Spanish",
-//         we: ,
-//         wish: ,
-//         you: ,
-//         a: ,
-//         merry: ,
-//         christmas: ,
-//         happy: ,
-//         new: ,
-//         year: ,
-//         have: ,
-//     },
-//     {
-//         id: "Italian",
-//         we: ,
-//         wish: ,
-//         you: ,
-//         a: ,
-//         merry: ,
-//         christmas: ,
-//         happy: ,
-//         new: ,
-//         year: ,
-//         have: ,
-//     },
-// ];
-
-// var germButton = document.getElementById("gb");
-// germButton.addEventListener("click", awesomeTranslator(germanObject));
-
-var germButton = document.getElementById("gb");
-let spanButton = document.getElementById('sb');
-let italButton = document.getElementById('ib');
-
-germButton.addEventListener("click", function(){awesomeTranslator(germanObject)});
-spanButton.addEventListener("click", function(){awesomeTranslator(spanishObject)});
-italButton.addEventListener("click", function(){awesomeTranslator(italianObject)});
-
-
-// germButton.addEventListener("click", (event) => {
-//     awesomeTranslator(germanObject);
-// });
-
-const germanObject = {
+const gb = {
     id: "German",
     we: "wir",
     wish: "wunsch",
@@ -76,7 +12,7 @@ const germanObject = {
     year: "jahr",
 };
 
-const spanishObject = {
+const sb = {
     id: "Spanish",
     we: "nosotros",
     wish: "deseo",
@@ -89,7 +25,7 @@ const spanishObject = {
     year: "nuevo",
 };
 
-const italianObject = {
+const ib = {
     id: "Italian",
     we: "noi",
     wish: "desiderio",
@@ -101,6 +37,12 @@ const italianObject = {
     new: "anno",
     year: "nuovo",
 };
+
+var dictionaries = {
+    ib, 
+    sb, 
+    gb
+}
 
 function awesomeTranslator (language) {
     var translatedText = document.getElementById('newWords');
@@ -115,3 +57,30 @@ function awesomeTranslator (language) {
         }
     });
 }
+
+const convertEvent = (e) => {
+    var language = window.dictionaries[e.target.id];
+    awesomeTranslator(language);
+}
+
+
+var germButton = document.getElementById("gb");
+let spanButton = document.getElementById('sb');
+let italButton = document.getElementById('ib');
+
+germButton.addEventListener("click", convertEvent);
+spanButton.addEventListener("click", convertEvent);
+italButton.addEventListener("click", convertEvent);
+
+
+// germButton.addEventListener("click", (event) => {
+//     awesomeTranslator(germanObject);
+// });
+
+
+
+
+
+
+
+
